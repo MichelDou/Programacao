36ad6e9f2712f323ddb30780efab6be6916e54d5
MichelDou <89814250+MichelDou@users.noreply.github.com>
2021-09-13 20:49:44 -0300

    Add files via upload

---
+++ ex22w.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+ int numero, i;
+printf("digite somente numero positivo");
+scanf("%i", &numero);
+
+while (numero<=0)
+
+{
+   printf("numero invalido! escreva denovo! digite novo numero:");
+   scanf("%i", &numero);
+}
+
+
+{
+    printf("Vielen Danke!");
+}
+
+
+
+return 0;
+}
