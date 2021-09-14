5969a6d303bd5ebcc7b3141082876771c1744d33
MichelDou <89814250+MichelDou@users.noreply.github.com>
2021-09-13 21:31:38 -0300

    Add files via upload

---
+++ ex24.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+ char sexo;
+
+printf("digite o sexo:");
+scanf("%s", &sexo);
+
+
+while ((sexo !='M')  &&  (sexo !='F')){
+
+printf("Erro, caractere errado! digite o sexo novamente:");
+scanf("%s", &sexo);
+
+
+}
+
+
+
+
+
+{
+    printf("Vielen Danke!");
+}
+
+
+
+return 0;
+}
