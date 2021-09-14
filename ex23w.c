a8a76253a565faf93cbadfc2271c11c6c3abec0b
MichelDou <89814250+MichelDou@users.noreply.github.com>
2021-09-13 21:22:55 -0300

    Add files via upload

---
+++ ex23w.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+ int valor1, valor2;
+printf("digite o primeiro valor");
+scanf("%i", &valor1);
+
+printf("digite o segundo valor");
+scanf("%i", &valor2);
+
+while (valor2<valor1)
+
+{
+   printf("digite novamente os valores! digite o valor1:");
+   scanf("%i", &valor1);
+
+   printf("digite novamente os valores! digite o valor2:");
+   scanf("%i", &valor2);
+
+}
+
+    
+
+
+
+{
+    printf("Vielen Danke!");
+}
+
+
+
+return 0;
+}
