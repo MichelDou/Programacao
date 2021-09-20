98f6005b89e461dfcb520971c9f172d6938b27e6
MichelDou <89814250+MichelDou@users.noreply.github.com>
2021-09-20 20:16:54 -0300

    Add files via upload

---
+++ Ex27f.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+ int numero, i, r,B;
+
+printf("digite um numero positivo:");
+scanf("%i", &numero);
+
+
+while (numero<=0){
+
+printf("Erro, caractere errado! digite o sexo novamente:");
+scanf("%i", &numero);
+
+}
+
+printf("digite o intervalo da tabuada:");
+scanf("%i", &B);
+
+
+while (B<=numero){
+
+printf("Erro, caractere errado! digite o segundo numero novamente:");
+scanf("%i", &B);
+}
+
+
+for (i=B;i<=10;i=i+B){
+r=numero*i;
+printf("%i x %i = %i\n", numero, i, r);
+
+}
+
+
+
+{
+    printf("Vielen Danke!");
+}
+
+
+
+return 0;
+}
