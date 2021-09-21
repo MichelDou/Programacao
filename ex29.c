be1dc8df031576a2584818d48e6a920da127d5f4
MichelDou <89814250+MichelDou@users.noreply.github.com>
2021-09-20 21:51:17 -0300

    Add files via upload

---
+++ ex29.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+ int numero, i, r, B;
+
+printf("digite o numero 1 para obter o resultado:");
+scanf("%i", &numero);
+
+
+while (numero!=1){
+
+printf("Erro, caractere errado! digite o segundo numero novamente:");
+scanf("%i", &numero);
+}
+
+i=1;
+for (int i=1;r<=100;numero++){
+r=numero+i;
+printf("%i\n",  r);
+
+}
+
+
+
+{
+    printf("Vielen Danke!");
+}
+
+
+
+return 0;
+}
