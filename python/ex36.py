cfe2382dcf9efe3a3c604537d3bf3d630f7ba49c
MichelDou <89814250+MichelDou@users.noreply.github.com>
2021-11-21 17:23:46 -0300

    Add files via upload

---
+++ python/ex36.py
@@ -0,0 +1,20 @@
+print('imprimir o maior de 10 numeros')
+ 
+numeros = []
+numeros2 = []
+maior=0
+result=0
+
+for i in range(0, 3, 1):
+     
+     n = int(input('Digite os numeros: '))
+     numeros.append(n)
+
+     
+
+mult=int(input("digite o numero multiplicativo:"))
+        
+print('Os resultados são:')
+for j in range(0, 3, 1):
+   numeros2.append(i)   
+print(numeros2[j]*mult)
